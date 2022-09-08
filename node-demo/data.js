7bf5c2b4616ce7f66860c48b587f7847cf0f59db
MohammadWaris3336 <112970154+MohammadWaris3336@users.noreply.github.com>
2022-09-09 04:35:20 +0500

    express js api and tested on postman Get Method

---
+++ node-demo/data.js
@@ -0,0 +1,6 @@
+const data= [
+    {name: 'anil', email:'anil@gmail.com', age:20},
+{name: 'Ali', email:'ali@gmail.com', age: 30},
+{name: 'umer', email:'umer@gmail.com', age: 29}
+];
+module.exports=data;
--- node-demo/expressapi.js
+++ node-demo/expressapi.js
@@ -2,15 +2,30 @@ const express = require('express');
 const app= express();
 
 app.get('',(req, res)=>{
-    res.send("Hello! This is Home Page");
+    res.send(`<h1>Hello! This is Home Page</h1>
+            <a href="/about">Got TO About Page</a>
+            <a href="/help">Got TO Help Page</a>
+    `);
 });
 
 app.get('/about', (req, res)=>{
-    res.send("Hello! This is About Page");
+    // console.log("welcome, "+req.query.name);
+    // res.send("welcome, "+req.query.name);
+    res.send(`
+    <input type="text" placeholder="Name" value="${req.query.name}">
+    <input type="submit" value="Submit">
+    <a href="/">Got TO Home Page</a>
+    <a href="/help">Got TO Help Page</a>
+    `);
 });
 
 app.get('/help', (req, res)=>{
-    res.send("Hello! This is help Page!");
+    res.send([{
+        name: "Ali", age: 22, Education: "BSCS"
+    },
+    {name: "umer", age: 32, Education: "BSCa"}
+]
+    );
 });
 
-app.listen(3306);
+app.listen(3305);
