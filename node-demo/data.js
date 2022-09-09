001d71a6762f37cbc4ddf544ea6626057ee731ef
MohammadWaris3336 <112970154+MohammadWaris3336@users.noreply.github.com>
2022-09-10 04:21:44 +0500

    checking

--- node-demo/data.js
+++ node-demo/data.js
@@ -1,6 +1,7 @@
 const data= [
     {name: 'anil', email:'anil@gmail.com', age:20},
 {name: 'Ali', email:'ali@gmail.com', age: 30},
-{name: 'umer', email:'umer@gmail.com', age: 29}
+{name: 'umer', email:'umer@gmail.com', age: 29},
+{name: 'umersss', email:'umer@gmail.com', age: 29}
 ];
 module.exports=data;
